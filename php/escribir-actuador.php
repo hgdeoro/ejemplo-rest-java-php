fce69504a97613d939f375d6d551f8cdabb640a9
Horacio G. de Oro <hgdeoro@gmail.com>
2014-02-22 12:57:00 -0300

    Agrega ejemplo de como enviar JSON en POST desde PHP y utilizarlo en Java

--- php/escribir-actuador.php
+++ php/escribir-actuador.php
@@ -6,8 +6,16 @@ use Guzzle\Http\Client;
 
 $client = new Client('http://localhost:8080');
 
-$request = $client->post('/tesis/actuador/32/valor?value=77');
-// echo $request->getUrl();
+// -- ORIGINAL: sin json
+// $request = $client->post('/tesis/actuador/32/valor?value=77');
+
+// -- ALTERNATIVA: no funciono
+//$request = $client->post('/tesis/actuador/32/valor', null, array(
+//	'valor' => 44
+//));
+
+$request = $client->post('/tesis/actuador/32/valor');
+$request->setBody(json_encode(array('valor' => 44)), 'application/json');
 
 // You must send a request in order for the transfer to occur
 $response = $request->send();
--- src/main/java/com/example/Actuador.java
+++ src/main/java/com/example/Actuador.java
@@ -2,12 +2,12 @@ package com.example;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,12 +25,19 @@ public class Actuador {
 	@POST
 	@Path("/valor")
 	@Produces(MediaType.APPLICATION_JSON)
-	public JsonObject escribirEnActuador(@PathParam("id_actaudor") String idActuador,
-			@QueryParam("value") String value) {
-		Integer idActuadorInt = Integer.parseInt(idActuador);
-		Integer valueInt = Integer.parseInt(value);
-		this.arduino.cambiarActuador(idActuadorInt.intValue(), valueInt.intValue());
-		return Json.createObjectBuilder().add("idActuador", idActuadorInt).add("valor", valueInt).build();
+	@Consumes(MediaType.APPLICATION_JSON)
+	public JsonObject escribirEnActuador(@PathParam("id_actaudor") String idActuador, JsonObject body) {
+		try {
+			Integer idActuadorInt = Integer.parseInt(idActuador);
+			// Integer valueInt =
+			// Integer.parseInt(body.get("valor").toString());
+			Integer valueInt = body.getInt("valor");
+			this.arduino.cambiarActuador(idActuadorInt.intValue(), valueInt.intValue());
+			return Json.createObjectBuilder().add("idActuador", idActuadorInt).add("valor", valueInt).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	@GET
