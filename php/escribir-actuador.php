100870583ef4dee1bde9ce7a47119708c30166d6
Horacio G. de Oro <hgdeoro@gmail.com>
2014-02-21 23:15:07 -0300

    Agrega ejemplo de PHP para hacer post

---
+++ php/escribir-actuador.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Guzzle\Http\Client;
+
+$client = new Client('http://localhost:8080');
+
+$request = $client->post('/tesis/actuador/32/escribir?value=77');
+// echo $request->getUrl();
+
+// You must send a request in order for the transfer to occur
+$response = $request->send();
+
+echo $response->getBody();
+
+// $data = $response->json();
+// echo $data['type'];
+
+?>
