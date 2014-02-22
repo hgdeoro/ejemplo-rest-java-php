1b776ec473ac3a7ad2fe45f47a77e4ea975495a5
Horacio G. de Oro <hgdeoro@gmail.com>
2014-02-21 22:00:55 -0300

    Elimina MyResource, actualiza tests

---
+++ src/test/java/com/example/SensorTest.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SensorTest {
+
+	private HttpServer server;
+	private WebTarget target;
+
+	@Before
+	public void setUp() throws Exception {
+		// start the server
+		server = Main.startServer();
+		// create the client
+		Client c = ClientBuilder.newClient();
+
+		// uncomment the following line if you want to enable
+		// support for JSON in the client (you also have to uncomment
+		// dependency on jersey-media-json module in pom.xml and
+		// Main.startServer())
+		// --
+		// c.configuration().enable(new
+		// org.glassfish.jersey.media.json.JsonJaxbFeature());
+
+		target = c.target(Main.BASE_URI);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		server.stop();
+	}
+
+	@Test
+	public void testLecturaSensor() {
+		String responseMsg = target.path("/sensor/33/leer").request().get(String.class);
+		assertTrue(responseMsg.contains("Lectura de sensor '33' devolvio"));
+	}
+}
