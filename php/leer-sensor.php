7361c0151311cf470cf3b13b36e058c95cea1c8c
Horacio G. de Oro <hgdeoro@gmail.com>
2014-02-21 23:02:24 -0300

    Ejemplo PHP para leer sensor

---
+++ php/leer-sensor.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Guzzle\Http\Client;
+
+$client = new Client('http://localhost:8080');
+
+$request = $client->get('/tesis/sensor/32/leer');
+// echo $request->getUrl();
+
+// You must send a request in order for the transfer to occur
+$response = $request->send();
+
+echo $response->getBody();
+
+// $data = $response->json();
+// echo $data['type'];
+
+?>
